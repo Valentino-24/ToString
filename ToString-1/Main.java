f58f76df3f39cd2f1d138a8e8b10d934f46ac736
Valentino <valentinolorca@gmail.com>
2025-09-02 09:13:50 -0300

    Subiendo archivos

--- ToString-1/Main.java
+++ ToString-1/Main.java
@@ -1,10 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[]args){
-        Estudiante e1 =  Estudiante.builder()
+        List<Estudiante>listaEstudiantes=new ArrayList<>();
+        listaEstudiantes.add(Estudiante.builder()
                 .nombre("Facundo Ganem")
                 .edad(19)
                 .carrera("Ingeniería en Sistemas")
-                .build();
-        System.out.println(e1);
+                .build());
+        listaEstudiantes.add(Estudiante.builder()
+                .nombre("Enzo Severino")
+                .edad(20)
+                .carrera("Redstone Engineer")
+                .build());
+        listaEstudiantes.add(Estudiante.builder()
+                .nombre("Jerónimo Zúñiga")
+                .edad(21)
+                .carrera("Médico Militar")
+                .build());
+        System.out.println(listaEstudiantes);
     }
 }
